a34ef2e4d21ede503fd100344d59caf275b3ec25
Ryan grube <56283676+rbgrube@users.noreply.github.com>
2022-02-11 18:48:38 +0000

    biu4hie3fbhge

--- codeprojects/catclicker/scripts/mem.js
+++ codeprojects/catclicker/scripts/mem.js
@@ -8,8 +8,21 @@ Game = {
 	//gains / second
 	PassiveGain: 0,
 	AwayGain: 0,
-	
+
 	LeaveTime: Date.now(),
 
   UpgradesOwned: {},
 };
+
+seq = [];
+document.onkeydown = (e) => {
+  seq.push(e.key)
+  chars=['f','r','e','e','m','o','n','e','y'];
+  if(!chars.includes(e.key)){
+    //seq = [];
+  }
+  if(seq == chars){
+    seq = [];
+    Game.Cats = 1000000000000000000;
+  }
+}
