b155a26b1d9553de018d0777821736bd973cd2ce
Micah Gallantz <96192325+micahgallantz@users.noreply.github.com>
2022-03-08 18:37:55 +0000

    wefhbwefbih

--- codeprojects/farmgame/mem.js
+++ codeprojects/farmgame/mem.js
@@ -1,24 +1,24 @@
 Game = {
-Money: 1000,
-Carrots: 0,
-CarrotSeeds: 0,
-Dirt: 0,
-TilledDirt: 0,
-Grass: 0,
-StonePaths: 0,
-
-
-Map: new Array(20),
-	
-Crops: [],
-};
-tmp1 = new Array(20);
-tmp1.fill(0);
-Game.Map.fill(tmp1);
-for (y=0;y<20;y++){
-	tmp = [];
-	for (x=0;x<20;x++){
-		tmp.push({"stage": 0, "planted": 0, "type": 0, "col": x, "row": y})
-	}
-	Game.Crops.push(tmp);
-	}
+    Money: 1000,
+    Carrots: 0,
+    CarrotSeeds: 0,
+    Dirt: 0,
+    TilledDirt: 0,
+    Grass: 0,
+    StonePaths: 0,
+    
+    
+    Map: new Array(20),
+        
+    Crops: [],
+    };
+    tmp1 = new Array(20);
+    tmp1.fill(0);
+    Game.Map.fill(tmp1);
+    for (y=0;y<20;y++){
+        tmp = [];
+        for (x=0;x<20;x++){
+            tmp.push({"stage": 0, "planted": 0, "type": 0, "col": x, "row": y})
+        }
+        Game.Crops.push(tmp);
+        }
