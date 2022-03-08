71d6e37eb0a762d14bb8b03e02851375b13cd5ff
Micah Gallantz <96192325+micahgallantz@users.noreply.github.com>
2022-03-08 17:51:22 +0000

    Diamond Cove 1.0

--- codeprojects/farmgame/store.js
+++ codeprojects/farmgame/store.js
@@ -1,7 +1,11 @@
-const store = document.getElementById('store');
-const button = document.getElementById("storeButton");
-const input = document.getElementById("input");
+var store = document.getElementById('store');
+var button = document.getElementById("storeButton");
+var input = document.getElementById("input");
+
 function storeOpen(){
+store = document.getElementById('store');
+button = document.getElementById("storeButton");
+input = document.getElementById("input");
 store.style.display = "block";
 button.style.display = "none";
 canvas.style.display = "none";
@@ -9,6 +13,9 @@ input.style.display = "none";
 }
 
 function storeClose(){
+store = document.getElementById('store');
+button = document.getElementById("storeButton");
+input = document.getElementById("input");
 store.style.display = "none";
 button.style.display = "block";
 canvas.style.display = "block";
