6272363b1a347e39e3c391926440b59c72c4d279
micahgallantz <96192325+micahgallantz@users.noreply.github.com>
2022-04-09 19:01:17 -0600

    andromeda

---
+++ codeprojects/Andromeda/scripts/script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
