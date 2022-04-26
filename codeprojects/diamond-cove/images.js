9a208eed7413c31b9e33bf703a4c0e8dd9fb3c3e
Micah Gallantz <96192325+micahgallantz@users.noreply.github.com>
2022-04-26 15:34:23 +0000

    reeeeeeet

--- codeprojects/diamond-cove/images.js
+++ codeprojects/diamond-cove/images.js
@@ -1,58 +1,58 @@
 var w = h = canvas.width = canvas.height = 640;
 
 const grass = new Image();
-grass.src = "./tex/grass.png";
+grass.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/grass.png";
 
 const moneytex = new Image();
-moneytex.src = "./tex/money.png";
+moneytex.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/money.png";
 
 const carrotSeedHotbar = new Image();
-carrotSeedHotbar.src = "./tex/carrotSeedHotbar.png";
+carrotSeedHotbar.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/carrotSeedHotbar.png";
 
 const carrotCount = new Image();
-carrotCount.src = "./tex/carrotCount.png";
+carrotCount.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/carrotCount.png";
 
 const carrotSeed = new Image();
-carrotSeed.src = "./tex/carrotSeed.png";
+carrotSeed.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/carrotSeed.png";
 
 const stonePath = new Image();
-stonePath.src = "./tex/stonePath.png";
+stonePath.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/stonePath.png";
 
 const dirt = new Image();
-dirt.src = "./tex/dirt.png";
+dirt.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/dirt.png";
 
 const tilledDirt = new Image();
-tilledDirt.src = "./tex/tilledDirt.png";
+tilledDirt.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/tilledDirt.png";
 
 const carrot = new Image();
-carrot.src = "./tex/carrot.png";
+carrot.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/carrot.png";
 
 const carrotStage1 = new Image();
-carrotStage1.src = "./tex/carrotStage1.png";
+carrotStage1.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/carrotStage1.png";
 
 const carrotStage2 = new Image();
-carrotStage2.src = "./tex/carrotStage2.png";
+carrotStage2.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/carrotStage2.png";
 
 const curser = new Image();
-curser.src = "./tex/curser.png";
+curser.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/curser.png";
 
 const hotbar_tex = new Image();
-hotbar_tex.src = "./tex/hotbar.png";
+hotbar_tex.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/hotbar.png";
 
 const hotbarSelected = new Image();
-hotbarSelected.src = "./tex/hotbarSelected.png";
+hotbarSelected.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/hotbarSelected.png";
 
 const carrotHotbar = new Image();
-carrotHotbar.src = "./tex/carrotHotbar.png";
+carrotHotbar.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/carrotHotbar.png";
 
 const stonePathHotbar = new Image();
-stonePathHotbar.src = "./tex/storePathHotbar.png";
+stonePathHotbar.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/storePathHotbar.png";
 
 const dirtHotbar = new Image();
-dirtHotbar.src = "./tex/dirtHotbar.png";
+dirtHotbar.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/dirtHotbar.png";
 
 const grassHotbar = new Image();
-grassHotbar.src = "./tex/grassHotbar.png";
+grassHotbar.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/grassHotbar.png";
 
 const tilledDirtHotbar = new Image();
-tilledDirtHotbar.src = "./tex/tilledDirtHotbar.png";
+tilledDirtHotbar.src = "http://micahgallantz.com/codeprojects/diamond-cove/tex/tilledDirtHotbar.png";
